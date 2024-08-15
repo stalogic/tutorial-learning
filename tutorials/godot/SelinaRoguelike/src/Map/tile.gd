7b99f36ae9a5118fe1cd4b146018ca13ec3f36ba
stalogic <jiangmings1992@163.com>
2024-08-15 21:33:40 +0800

    完成Roguelike Part4的学习

--- tutorials/godot/SelinaRoguelike/src/Map/tile.gd
+++ tutorials/godot/SelinaRoguelike/src/Map/tile.gd
@@ -3,18 +3,32 @@ extends Sprite2D
 
 var _definition: TileDefinition
 
+var is_explored: bool = false:
+	set(value):
+		is_explored = value
+		if is_explored and not visible:
+			visible = true
+
+var is_in_view: bool = false:
+	set(value):
+		is_in_view = value
+		modulate = _definition.color_lit if is_in_view else _definition.color_dark
+		if is_in_view and not is_explored:
+			is_explored = true
+
 func _init(grid_position: Vector2i, tile_definition: TileDefinition) -> void:
-	self.centered = false
-	self.position = Grid.grid_to_world(grid_position)
-	self.set_tile_type(tile_definition)
+	visible = false
+	centered = false
+	position = Grid.grid_to_world(grid_position)
+	set_tile_type(tile_definition)
 
 func set_tile_type(tile_definition: TileDefinition) -> void:
-	self._definition = tile_definition
-	self.texture = self._definition.texture
-	self.modulate = self._definition.color_dark
+	_definition = tile_definition
+	texture = _definition.texture
+	modulate = _definition.color_dark
 
 func is_walkable() -> bool:
-	return self._definition.is_walkable
+	return _definition.is_walkable
 	
 func is_transparent() -> bool:
-	return self._definition.is_transparent
+	return _definition.is_transparent
